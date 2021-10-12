fbc8c8ca8a01c0f11fd8e0004dece9cc0119928e
95anita6 <38220315+95anita6@users.noreply.github.com>
2021-10-12 19:52:28 +0530

    Initial commit

---
+++ app_test.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+import logging
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
+@app.route("/")
+def home():
+    app.logger.info('Info level log')
+    app.logger.warning('Warning level log')
+    app.logger.critical('Critical level log')
+    app.logger.error('Error level log')
+    return 'Welcome to the App!'  
+
+if __name__ == "__main__":
+    app.run(debug=True)
